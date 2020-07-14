18680c8d0d875c2ca55db038574b2dc72457ead1
Lukasz Kaminski <lukasz.kaminski@vimn.com>
2020-07-14 18:10:52 +0200

    Parsing csv data: * obtaining messages and categories from csv files * combaining data into dataframe * saving data into database

--- app/run.py
+++ app/run.py
@@ -6,14 +6,13 @@ from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 
 from flask import Flask
-from flask import render_template, request, jsonify
+from flask import render_template, request
 from plotly.graph_objs import Bar
-import sklearn.externals
 import joblib
 from sqlalchemy import create_engine
 
-
 app = Flask(__name__)
+
 
 def tokenize(text):
     tokens = word_tokenize(text)
@@ -26,9 +25,10 @@ def tokenize(text):
 
     return clean_tokens
 
+
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/Messages.db')
+df = pd.read_sql_table('MessageTable', engine)
 
 # load model
 model = joblib.load("../models/your_model_name.pkl")
@@ -38,12 +38,11 @@ model = joblib.load("../models/your_model_name.pkl")
 @app.route('/')
 @app.route('/index')
 def index():
-    
     # extract data needed for visuals
     # TODO: Below is an example - modify to extract data for your own visuals
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
-    
+
     # create visuals
     # TODO: Below is an example - modify to create your own visuals
     graphs = [
@@ -66,20 +65,20 @@ def index():
             }
         }
     ]
-    
+
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
-    graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
+    graph_json = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+
     # render web page with plotly graphs
-    return render_template('master.html', ids=ids, graphJSON=graphJSON)
+    return render_template('master.html', ids=ids, graphJSON=graph_json)
 
 
 # web page that handles user query and displays model results
 @app.route('/go')
 def go():
     # save user input in query
-    query = request.args.get('query', '') 
+    query = request.args.get('query', '')
 
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,30 @@
 import sys
+from sqlalchemy import create_engine
+import pandas as pd
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+
+    df = pd.merge(left=messages, right=categories, left_on='id', right_on='id', how='outer')
+
+    categories = df.categories.str.split(";", expand=True)
+    row = categories.iloc[1]
+    category_colnames = row.replace(regex=r'(-\d)', value='')
+    categories.columns = category_colnames
+    categories = categories.replace(regex='([a-z_]+-)', value='').astype(int)
+    df.drop('categories', 1, inplace=True)
+    return pd.concat([df, categories], axis=1, join='outer')
 
 
 def clean_data(df):
-    pass
+    return df.drop_duplicates()
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine("sqlite:///{0}".format(database_filename))
+    df.to_sql('MessageTable', engine, index=False)
 
 
 def main():
@@ -22,20 +36,22 @@ def main():
               .format(messages_filepath, categories_filepath))
         df = load_data(messages_filepath, categories_filepath)
 
+        print(df.shape)
+
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
-              'to as the third argument. \n\nExample: python process_data.py '\
-              'disaster_messages.csv disaster_categories.csv '\
+        print('Please provide the filepaths of the messages and categories '
+              'datasets as the first and second argument respectively, as '
+              'well as the filepath of the database to save the cleaned data '
+              'to as the third argument. \n\nExample: python process_data.py '
+              'disaster_messages.csv disaster_categories.csv '
               'DisasterResponse.db')
 
 
